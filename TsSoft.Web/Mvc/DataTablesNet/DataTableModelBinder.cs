3fa72b5e89a005f091ebe90a08ae990bf1d2e28d
Alexey Trofimov <a.trofimov@ts-soft.ru>
2014-11-20 11:03:14 +0300

    Support for an entirely new API available in DataTables 1.10

--- TsSoft.Web/Mvc/DataTablesNet/DataTableModelBinder.cs
+++ TsSoft.Web/Mvc/DataTablesNet/DataTableModelBinder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace TsSoft.Web.Mvc.DataTablesNet
@@ -8,28 +10,40 @@ namespace TsSoft.Web.Mvc.DataTablesNet
     /// TODO
     /// </summary>
     /// <author>Eugene Yaroslavov</author>
+    /// <author>Alexey Trofimov</author>
     public class DataTableModelBinder : DefaultModelBinder
     {
         public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
             var request = controllerContext.HttpContext.Request;
             var model = (DataTableSettings)base.BindModel(controllerContext, bindingContext);
-            model.RequestId = Convert.ToInt32(request["sEcho"]);
-            model.Take = Convert.ToInt32(request["iDisplayLength"]);
-            model.Skip = Convert.ToInt32(request["iDisplayStart"]);
+            model.RequestId = Convert.ToInt32(request["draw"]);
+            model.Take = Convert.ToInt32(request["length"]);
+            model.Skip = Convert.ToInt32(request["start"]);
 
+            const string columnIndexGroupName = "columnIndex";
+            var columnOrderPattern = new Regex(string.Format(@"order\[(?<{0}>\d+)\]\[column\]", columnIndexGroupName));
+            var sortColumns = request.Form.AllKeys.Where(k => columnOrderPattern.IsMatch(k))
+                .ToDictionary(k => int.Parse(columnOrderPattern.Match(k).Groups[columnIndexGroupName].Value), k => k);
             var columns = new List<SortColumn>();
-            int sortColumnCount = Convert.ToInt32(request["iSortingCols"]);
-            for (int i = 0; i < sortColumnCount; i++)
+            foreach (var sortColumn in sortColumns.OrderBy(k => k.Key))
             {
-                var column = new SortColumn();
-                column.Index = Convert.ToInt32(request["iSortCol_" + i]);
-                column.Name = request["mDataProp_" + column.Index];
-                var sortOrder = request["sSortDir_" + i];
-                column.Order = !string.IsNullOrWhiteSpace(sortOrder) && sortOrder.ToUpper() == "DESC"
-                    ? SortOrder.Descending : SortOrder.Ascending;
-                columns.Add(column);
+                var isOrderable = bool.Parse(request.Form[string.Format("columns[{0}][orderable]", sortColumn.Key)]);
+                if (!isOrderable)
+                {
+                    continue;
+                }
+
+                var columnName = request.Form[string.Format("columns[{0}][name]", sortColumn.Key)];
+                var direction = request.Form[string.Format("order[{0}][dir]", sortColumn.Key)];
+                columns.Add(new SortColumn
+                {
+                    Index = sortColumn.Key,
+                    Name = columnName,
+                    Order = direction == "desc" ? SortOrder.Descending : SortOrder.Ascending,
+                });
             }
+
             model.SortColumns = columns.ToArray();
             return model;
         }
